253023420c32abea2b63b98da3f229ee8a462b2d
Scott Davis <jetviper21@gmail.com>
2012-11-01 19:15:30 -0400

    set correct path

--- lib/compass-blueprint.rb
+++ lib/compass-blueprint.rb
@@ -1,8 +1,4 @@
 require 'compass'
 require "compass-blueprint/version"
 
-framework_dir = File.join(File.dirname(__FILE__), '..', 'framework')
-
-Compass::Frameworks.register('blueprint',
-  :stylesheets_directory => File.join(framework_dir, 'stylesheets'),
-  :templates_directory => File.join(framework_dir, 'templates'))
+Compass::Frameworks.register('blueprint', :path => File.join(File.dirname(__FILE__), '..', 'framework', 'blueprint'))
